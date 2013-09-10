cdec43656b00d5d43c8c2af619a3450d350af809
brito <brito>
2013-09-10 15:03:33 +0000

    debug em erhandler de novo
    git-svn-id: http://svn.brlight.net/svn/lightbase-neo/trunk/LBConverter@556 29b92fdf-8c97-4584-b987-84e8d3c556fa

--- lbconverter/scripts/erhandler.py
+++ lbconverter/scripts/erhandler.py
@@ -9,15 +9,16 @@ def is_error(url, jsonfull):
     except:
       logger.debug(jsonfull)
       logger.debug("em " + url)
-    try:
-      a = len(json)
-    except TypeError:
-      id_reg = str(json)
-      logger.info('Escrito com sucesso id_reg: ' + id_reg)
     else:
-      msg = json['_error_message']
-      n_err = str(json['_status'])
-      logger.error(n_err + ': ' + msg + " em " + url)
+      try:
+        a = len(json)
+      except TypeError:
+        id_reg = str(json)
+        logger.info('Escrito com sucesso id_reg: ' + id_reg)
+      else:
+        msg = json['_error_message']
+        n_err = str(json['_status'])
+        logger.error(n_err + ': ' + msg + " em " + url)
 
 
 logger = logging.getLogger("LBConverter")
